6a65b312a2276bf27ee070b3c54987f55f616ebd
Cote Phinnizee <ucphinni@gmail.com>
2022-04-17 11:33:15 -0400

    ci

--- rpizp/presetup.sh
+++ rpizp/presetup.sh
@@ -5,7 +5,7 @@ tceload='tce-load'
 pythonpkg='python3.9'
 tcedir='/mnt/sda1/tce'
 tmpdir="$HOME/tmp"
-mkdir -p "$tmpdir"
+mkdir -p $tmpdir
 export PYPPETEER_HOME="/usr/local/share/pyppetteer"
 $tceload -w ntpclient
 $tceload -w $pythonpkg
@@ -35,9 +35,9 @@ md5sum ${pythonpkg}-extras_tmp.tcz > ${pythonpkg}-extras_tmp.tcz.md5.txt
 unsquashfs -ll -d '' ${pythonpkg}-extras_tmp.tcz | grep -v '^d' | sed -e 's#.* /#/#' -e 's# -> .*##' -e 1,3d > ${pythonpkg}-extras.tcz.list
 $tceload -il ./${pythonpkg}-extras_tmp.tcz
 
-mkdir -p "$PYPPETEER_HOME"
+mkdir -p $PYPPETEER_HOME
 /usr/local/bin/pyppeteer-install
-(cd "$PYPPETEER_HOME" && tar cf - . && cd .. && rm -rf "$PYPPETEER_HOME") | (cd "$tmpdir/pkg" && mkdir -p "$tmpdir/pkg/$PYPPETEER_HOME" && tar xpf -)
+(cd "$PYPPETEER_HOME" && tar cf - . && cd .. && rm -rf "$PYPPETEER_HOME") | (cd "$tmpdir/pkg" && mkdir -p $tmpdir/pkg/$PYPPETEER_HOME && tar xpf -)
 
 rm -f ${pythonpkg}-extras.tcz	
 sudo mksquashfs pkg/ ${pythonpkg}-extras.tcz
