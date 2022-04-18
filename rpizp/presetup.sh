c8a4cb123fc53ba1a2592c16cf51063902b57d95
Cote Phinnizee <ucphinni@gmail.com>
2022-04-18 10:31:12 -0400

    ci

--- rpizp/presetup.sh
+++ rpizp/presetup.sh
@@ -36,6 +36,6 @@ sudo chown tc:staff ${pythonpkg}-extras.tcz
 md5sum ${pythonpkg}-extras.tcz > ${pythonpkg}-extras.tcz.md5.txt
 unsquashfs -ll -d '' ${pythonpkg}-extras.tcz | grep -v '^d' | sed -e 's#.* /#/#' -e 's# -> .*##' -e 1,3d > ${pythonpkg}-extras.tcz.list
 $tceload -ic ./${pythonpkg}-extras.tcz
-cp ./${pythonpkg}-extras.tcz ${pythonpkg}-extras.tcz.md5.txt ${pythonpkg}.tcz.dep $tcedir/optional
+cp ./${pythonpkg}-extras.tcz ${pythonpkg}-extras.tcz.md5.txt ${pythonpkg}-extras.tcz.dep $tcedir/optional
 
 
