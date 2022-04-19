dcf9d30b4a9ccc05b071ee24c4b7486aeb073b8c
Cote Phinnizee <ucphinni@gmail.com>
2022-04-19 17:46:21 -0400

    ci

--- rpizp/presetup.sh
+++ rpizp/presetup.sh
@@ -1,58 +1,42 @@
 #!/bin/bash -xv
 
-python='python3.9'
 tceload='tce-load'
-pythonpkg='python3.9'
+nodepkg='node'
 tcedir='/mnt/sda1/tce'
 tmpdir="$HOME/tmp"
 mkdir -p $tmpdir
-export PYPPETEER_HOME="/usr/local/share/pyppeteer"
+
 $tceload -w ntpclient
-$tceload -w $pythonpkg
+$tceload -w firefox_getLatest
+$tceload -w node
 $tceload -w git
-$tceload -i ntpclient
-$tceload -i $pythonpkg
-$tceload -i git
-$tceload -w compiletc
-$tceload -w ${pythonpkg}-dev 
 $tceload -w squashfs-tools 
+$tceload -w compiletc
+$tceload -w squashfs-tools 
+$tceload -i ntpclient
+$tceload -i firefox_getLatest
+$tceload -i node
+$tceload -i git
+$tceload -i squashfs-tools 
 $tceload -il compiletc
-$tceload -il ${pythonpkg}-dev 
 $tceload -il squashfs-tools 
-$python -m ensurepip
-$python -m pip install  --upgrade pip
+
 /bin/rm -rf $tmpdir/pkg
 mkdir -p "$tmpdir/pkg"
-$python -m pip install pyppeteer aiohttp[speedups] --ignore-installed --root=$tmpdir/pkg
 
-( cd $tmpdir/pkg && find usr -type d -name __pycache__  | xargs rm -rf )
-( cd $tmpdir/pkg && find usr -type f -name "*.py[co]" | xargs rm -f )
 cd "$tmpdir"
-rm -f ${pythonpkg}-extras.tcz	
-sudo mksquashfs pkg/ ${pythonpkg}-extras.tcz
-sudo chown tc:staff ${pythonpkg}-extras.tcz 
-md5sum ${pythonpkg}-extras.tcz > ${pythonpkg}-extras.tcz.md5.txt
-unsquashfs -ll -d '' ${pythonpkg}-extras.tcz | grep -v '^d' | sed -e 's#.* /#/#' -e 's# -> .*##' -e 1,3d > ${pythonpkg}-extras.tcz.list
-$tceload -i ./${pythonpkg}-extras.tcz
-echo "${pythonpkg}.tcz" > ${pythonpkg}-extras.tcz.dep
-cp ./${pythonpkg}-extras.tcz ${pythonpkg}-extras.tcz.md5.txt ${pythonpkg}-extras.tcz.dep $tcedir/optional
 
 export PYPPETEER_HOME=/usr/local/share/pyppeteer
 sudo mkdir -p "$PYPPETEER_HOME"
 sudo chmod 777 "$PYPPETEER_HOME"
 /usr/local/bin/pyppeteer-install
-mkdir -p $tmpdir/pkg2$PYPPETEER_HOME
-tar c -f - -C "$PYPPETEER_HOME" .  | tar x -f - --numeric-owner -C "$tmpdir/pkg2"
+mkdir -p $tmpdir/pkg$PYPPETEER_HOME
+tar c -f - -C "$PYPPETEER_HOME" .  | tar x -f - --numeric-owner -C "$tmpdir/pkg"
 
-pyppeteerpkg='pyppeteer'
-
-sudo mksquashfs pkg2/ ${pyppeteerpkg}-extras.tcz
-sudo chown tc:staff ${pyppeteerpkg}-extras.tcz 
-md5sum ${pyppeteerpkg}-extras.tcz > ${pyppeteerpkg}-extras.tcz.md5.txt
-unsquashfs -ll -d '' ${pyppeteerpkg}-extras.tcz | grep -v '^d' | sed -e 's#.* /#/#' -e 's# -> .*##' -e 1,3d > ${pyppeteerpkg}-extras.tcz.list
-$tceload -i ./${pyppeteerpkg}-extras.tcz
-echo "${pythonpkg}-extras.tcz" > ${pyppeteerpkg}-extras.tcz.dep
-cp ./${pyppeteerpkg}-extras.tcz ${pyppeteerpkg}-extras.tcz.md5.txt ${pyppeteerpkg}-extras.tcz.dep $tcedir/optional
-
-
-
+sudo mksquashfs pkg/ ${nodepkg}-extras.tcz
+sudo chown tc:staff ${nodepkg}-extras.tcz 
+md5sum ${nodepkg}-extras.tcz > ${nodepkg}-extras.tcz.md5.txt
+unsquashfs -ll -d '' ${nodepkg}-extras.tcz | grep -v '^d' | sed -e 's#.* /#/#' -e 's# -> .*##' -e 1,3d > ${nodepkg}-extras.tcz.list
+$tceload -i ./${nodepkg}-extras.tcz
+echo "${nodepkg}.tcz" > ${nodepkg}-extras.tcz.dep
+cp ./${nodepkg}-extras.tcz ${nodepkg}-extras.tcz.md5.txt ${nodepkg}-extras.tcz.dep $tcedir/optional
