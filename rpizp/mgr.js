1fe1da7079b53047e54b3c3b3265fff3487637b0
Cote Phinnizee <ucphinni@gmail.com>
2022-04-22 14:35:00 -0400

    ci

---
+++ rpizp/mgr.js
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+const { writeFile } = require('fs');
+const { promisify } = require('util');
+
+const { Builder, By, Key, promise, until } = require('selenium-webdriver');
+const firefox = require('selenium-webdriver/firefox');
+
+promise.USE_PROMISE_MANAGER = false;
+
+
+const binary = new firefox.Binary('/usr/local/bin/firefox');
+const driver = new Builder()
+.forBrowser('firefox')
+.setFirefoxOptions(new firefox.Options().setBinary(binary))
+.build();
+
+async function main() {
+  await driver.get('https://developer.mozilla.org/');
+  await driver.findElement(By.id('home-q')).sendKeys('testing', Key.RETURN);
+  await driver.wait(until.titleIs('Search Results for "testing" | MDN'));
+  await driver.wait(async () => {
+    const readyState = await driver.executeScript('return document.readyState');
+    return readyState === 'complete';
+  });
+  const data = await driver.takeScreenshot();
+  await promisify(writeFile)('screenshot.png', data, 'base64');
+  await driver.quit();
+}
+
+main();
--- rpizp/presetup.sh
+++ rpizp/presetup.sh
@@ -1,57 +1,35 @@
 #!/bin/bash -xv
 
-pip='pip3.9'
-python='python3.9'
 tceload='tce-load'
-pythonpkg='python3.9'
+nodepkg='node'
 zoommt='zoommon'
 tmpdir="$HOME/tmp"
 mkdir -p "$tmpdir"
-export PYPPETEER_HOME="/usr/local/share/zoommon"
+export PYPPETEER_HOME="/usr/local/share/vmtmon"
+$tceload -w firefox-getLatest
 $tceload -w ntpclient
-$tceload -w $pythonpkg
+$tceload -w node
 $tceload -w git
-$tceload -w compiletc
-$tceload -w ${pythonpkg}-dev 
 $tceload -w squashfs-tools 
 $tceload -w rsync 
 
+$tceload -i firefox-getLatest
 $tceload -i ntpclient
-$tceload -i $pythonpkg
+$tceload -i node
 $tceload -i git
-$tceload -il compiletc
-$tceload -il ${pythonpkg}-dev 
 $tceload -il squashfs-tools 
 $tceload -il rsync
 
-if ! which $pipv
-then
-	$python -m ensurepip
-	$python -m pip install  --upgrade pip
-fi
-/bin/rm -rf $tmpdir/pkg
-mkdir -p $tmpdir/pkg
-$python -m pip install playwright aiohttp[speedups] --ignore-installed --root=$tmpdir/pkg
-
-( cd $tmpdir/pkg && find usr -type d -name __pycache__  | xargs rm -rf )
-( cd $tmpdir/pkg && find usr -type f -name "*.py[co]" | xargs rm -f )
-cd $tmpdir
-rm -f ${pythonpkg}-extras.tcz	
-sudo mksquashfs pkg/ ${pythonpkg}-extras.tcz
-sudo chown tc:staff ${pythonpkg}-extras.tcz 
-md5sum ${pythonpkg}-extras.tcz > ${pythonpkg}-extras.tcz.md5.txt
-unsquashfs -ll -d '' ${pythonpkg}-extras.tcz | grep -v '^d' | sed -e 's#.* /#/#' -e 's# -> .*##' -e 1,3d > ${pythonpkg}-extras.tcz.list
-$tceload -i ./${pythonpkg}-extras.tcz
-
 sudo /bin/rm -rf  "$PYPPETEER_HOME"
-sudo mkdir -p "$PYPPETEER_HOME/pw-browsers"
-sudo chown 777 "$PYPPETEER_HOME" "$PYPPETEER_HOME/pw-browsers"
+sudo chown 777 "$PYPPETEER_HOME"
+cd "PYPPETEER_HOME"
+npm i selenium-webdriver
+npm i geckodriver
 
-export PLAYWRIGHT_BROWSERS_PATH "$PYPPETEER_HOME/pw-browsers"
-$python -m playwright install
+
 rsync --append-verify --inplace --remove-source-files --delete -lptAXH --force "$PYPPETEER_HOME" $tmpdir/pkg2 
 rm -f ${zoommt}-extras.tcz	
-sudo mksquashfs pkg2/ ${zoommt}-extras.tcz
+sudo mksquashfs pkg/ ${zoommt}-extras.tcz
 sudo chown tc:staff ${zoommt}-extras.tcz 
 md5sum ${zoommt}-extras.tcz > ${zoommt}-extras.tcz.md5.txt
 unsquashfs -ll -d '' ${zoommt}-extras.tcz | grep -v '^d' | sed -e 's#.* /#/#' -e 's# -> .*##' -e 1,3d > ${zoommt}-extras.tcz.list
